8a03220c33cea79395b22ca2b0681732c08ce780
NibhaAggarwal <nibha1110@gmail.com>
2019-11-01 15:31:52 +0530

    code

---
+++ SimpleDemo/Extensions/Loader+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  Loader+Extension.swift
+//  SimpleDemo
+//
+//  Created by Nibha Aggarwal on 31/10/19.
+//  Copyright © 2019 Nibha Aggarwal. All rights reserved.
+//
+
+import Foundation
---
+++ SimpleDemo/Login/BiometricViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  BiometricViewModel.swift
+//  SimpleDemo
+//
+//  Created by Nibha Aggarwal on 01/11/19.
+//  Copyright © 2019 Nibha Aggarwal. All rights reserved.
+//
+
+import Foundation
