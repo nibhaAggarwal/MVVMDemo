382f090319963b2f7324e70a3ee97043967c6646
NibhaAggarwal <nibha1110@gmail.com>
2019-11-21 11:18:06 +0530

    commit

---
+++ SimpleDemo/CommonView/CommonVC.swift
@@ -0,0 +1,30 @@
+//
+//  CommonVC.swift
+//  SimpleDemo
+//
+//  Created by Nibha Aggarwal on 19/11/19.
+//  Copyright © 2019 Nibha Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class CommonVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
